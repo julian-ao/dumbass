6d3619ae65f1839573422067c857883fe29ba2c4
Julian <julianao@stud.ntnu.no>
2023-10-03 21:26:07 +0200

    test: tests for artist/song card
    added basic tests for the artist/song card for checking if the different elements renders correctly
    
    may need to add snapshot tests in the future
    
    issue: #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    test: {
+        globals: true,
+        environment: 'jsdom',
+    },
     plugins: [react()],
     server: {
         open: true,
