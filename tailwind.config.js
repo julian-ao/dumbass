640cb6dbca37c156db6c508df59c957e371b3f2a
Julian <julianao@stud.ntnu.no>
2023-10-05 14:31:21 +0200

    feat: pipeline
    linting stage
    
    issue: #6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {
-            colors: {
-                lightYellow: '#faf3dd',
-                tan: '#c8d5b9',
-                green: '#8fc0a9',
-                teal: '#89c1bd',
-                blueGray: '#696d7d',
-                lightGray: '#efefef',
-                white: '#ffffff',
-            },
-            fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-            },
+export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
+export const theme = {
+    extend: {
+        colors: {
+            lightYellow: '#faf3dd',
+            tan: '#c8d5b9',
+            green: '#8fc0a9',
+            teal: '#89c1bd',
+            blueGray: '#696d7d',
+            lightGray: '#efefef',
+            white: '#ffffff',
+        },
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
         },
     },
-    plugins: [],
-    darkMode: 'class',
 };
+export const plugins = [];
+export const darkMode = 'class';
