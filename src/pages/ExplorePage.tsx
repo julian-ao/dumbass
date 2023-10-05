456fbe98eb0cab97f1a8e79054be65be4c7640d1
Julian <julianao@stud.ntnu.no>
2023-10-05 13:26:36 +0200

    chore: lint
    linted all files using command
    
    issue: #10

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import SearchBar from '../components/molecules/SearchBar';
+import { useState } from 'react'
+import SearchBar from '../components/molecules/SearchBar'
 import ArtistSongCard, {
     ArtistCardProps,
-    SongCardProps,
-} from '../components/molecules/ArtistSongCard';
+    SongCardProps
+} from '../components/molecules/ArtistSongCard'
 
 export default function ExplorePage() {
-    const [filter, setFilter] = useState('Song');
+    const [filter, setFilter] = useState('Song')
     const FiftycentProps = {
         cardType: 'artist',
         imageUrl:
@@ -14,8 +14,8 @@ export default function ExplorePage() {
         title: '50 Cent',
         alternateNames: ['Fiddy', 'Boo Boo'],
         rating: 4.5,
-        numOfRatings: 1000000,
-    } as ArtistCardProps;
+        numOfRatings: 1000000
+    } as ArtistCardProps
     const InDaClubProps = {
         cardType: 'song',
         imageUrl:
@@ -24,29 +24,28 @@ export default function ExplorePage() {
         artist: '50 Cent',
         rating: 4.5,
         numOfRatings: 1000000,
-        releaseDate: '2003-01-07',
-    } as SongCardProps;
+        releaseDate: '2003-01-07'
+    } as SongCardProps
 
     return (
-        <div className="bg-[#EFEFEF] min-h-screen">
-            <div className="flex items-center justify-center h-16 bg-[#FAF3DD] drop-shadow"></div>
-            <div className="flex flex-col items-center justify-center w-screen px-12">
+        <div className='bg-[#EFEFEF] min-h-screen'>
+            <div className='flex items-center justify-center h-16 bg-[#FAF3DD] drop-shadow'></div>
+            <div className='flex flex-col items-center justify-center w-screen px-12'>
                 <SearchBar
-                    className="w-4/5 mt-10 drop-shadow mb-10"
+                    className='w-4/5 mt-10 drop-shadow mb-10'
                     filterOptions={['Song', 'Artist']}
                     selectedFilter={filter}
                     onFilterChange={(newFilter) => setFilter(newFilter)}
                 />
                 {/* Songs */}
-                <div className="w-3/4">
+                <div className='w-3/4'>
                     <h1
-                        data-testid="top_song_header"
-                        className="text-3xl font-bold mt-10 mb-10 text-blueGray self-start"
-                    >
+                        data-testid='top_song_header'
+                        className='text-3xl font-bold mt-10 mb-10 text-blueGray self-start'>
                         Top Songs
                     </h1>
                     {/* Rendering 6 cards */}
-                    <div className="mb-20 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 sm:gap-8 gap-4">
+                    <div className='mb-20 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 sm:gap-8 gap-4'>
                         {Array(6)
                             .fill(InDaClubProps)
                             .map((props, index) => (
@@ -56,15 +55,14 @@ export default function ExplorePage() {
                 </div>
 
                 {/* Artists */}
-                <div className="w-3/4">
+                <div className='w-3/4'>
                     <h1
-                        data-testid="top_artist_header"
-                        className="text-3xl font-bold mt-10 mb-10 text-blueGray self-start"
-                    >
+                        data-testid='top_artist_header'
+                        className='text-3xl font-bold mt-10 mb-10 text-blueGray self-start'>
                         Top Artists
                     </h1>
                     {/* Rendering 6 cards */}
-                    <div className="mb-20 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 sm:gap-8 gap-4">
+                    <div className='mb-20 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 sm:gap-8 gap-4'>
                         {Array(6)
                             .fill(FiftycentProps)
                             .map((props, index) => (
@@ -74,5 +72,5 @@ export default function ExplorePage() {
                 </div>
             </div>
         </div>
-    );
+    )
 }
