a8831e54bd780b83e232d96d0a9c2e477e055b41
linnerudsander <sandesl@stud.ntnu.no>
2023-10-11 17:47:35 +0200

    refactor: linting
    Issue: #24

--- src/components/molecules/CommonSearchBar.tsx
+++ src/components/molecules/CommonSearchBar.tsx
@@ -106,7 +106,11 @@ const CommonSearchBar = ({
                     </div>
                 )}
                 <button className='p-2 rounded-md ml-2' onClick={handleSearch}>
-                    <FaSearch size={20} color='#999' data-testid="search-icon" />
+                    <FaSearch
+                        size={20}
+                        color='#999'
+                        data-testid='search-icon'
+                    />
                 </button>
             </div>
             {showDropdown && (
