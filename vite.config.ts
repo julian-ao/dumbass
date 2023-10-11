0f56432fde74687316c5d1d8c7f88b7c583d4390
linnerudsander <sandesl@stud.ntnu.no>
2023-10-11 15:37:31 +0200

    refactor: remove unused import
    Issue: #11

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Svgr from '@svgr/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +8,7 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom'
     },
-    plugins: [react(), Svgr()],
+    plugins: [react()],
     server: {
         open: true
     }
