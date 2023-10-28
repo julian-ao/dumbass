b8818aaa43d98a87a439457b0eed908af0108533
linnerudsander <sandesl@stud.ntnu.no>
2023-10-28 11:28:51 +0200

    refactor: semantic html on CommonDropdown.tsx
    issue: #37

--- frontend/src/components/atoms/CommonDropdown.tsx
+++ frontend/src/components/atoms/CommonDropdown.tsx
@@ -48,12 +48,12 @@ const CommonDropdown = ({
 }: CommonDropdownProps) => {
     return (
         <Menu as='div' className='relative inline-block text-left h-full'>
-            <div className='h-full'>
+            <section className='h-full'>
                 <Menu.Button className='inline-flex justify-center items-center h-full w-full rounded-md px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-offset-2'>
                     {icon}
                     {label ? <Text ml={2}>{label}</Text> : selectedFilter}
                 </Menu.Button>
-            </div>
+            </section>
             <Transition
                 as={Fragment}
                 enter='transition ease-out duration-100'
@@ -63,7 +63,7 @@ const CommonDropdown = ({
                 leaveFrom='transform opacity-100 scale-100'
                 leaveTo='transform opacity-0 scale-95'>
                 <Menu.Items className='absolute left-0 z-10 mt-2 w-32 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                    <div className='py-1'>
+                    <section className='py-1'>
                         {filterOptions.map((option, index) => (
                             <Menu.Item key={option}>
                                 {({ active }) => (
@@ -86,7 +86,7 @@ const CommonDropdown = ({
                                 )}
                             </Menu.Item>
                         ))}
-                    </div>
+                    </section>
                 </Menu.Items>
             </Transition>
         </Menu>
