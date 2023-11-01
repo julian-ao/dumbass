d366818649afb557705cb4163142c956a69a49a6
Julian <julianao@stud.ntnu.no>
2023-11-01 13:12:06 +0100

    feat: errorPage component
    issue: #51

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { Toaster } from 'react-hot-toast'
 import { useState } from 'react'
@@ -9,11 +9,11 @@ import RegisterPage from './components/pages/RegisterPage'
 import HomePage from './components/pages/HomePage'
 import FavoritesPage from './components/pages/FavoritesPage'
 import Navbar from './components/organisms/Navbar'
-import NotFoundPage from './components/pages/NotFoundPage'
 import SearchPage from './components/pages/SearchPage'
 import { ArtistPage } from './components/pages/ArtistPage'
 import { SongPage } from './components/pages/SongPage'
 import 'react-loading-skeleton/dist/skeleton.css'
+import { ErrorPage } from './components/pages/ErrorPage'
 
 const queryClient = new QueryClient()
 const client = new ApolloClient({
@@ -23,6 +23,7 @@ const client = new ApolloClient({
 
 export default function App() {
     const [userLoggedIn, setUserLoggedIn] = useState(false)
+    const navigate = useNavigate()
 
     return (
         <ChakraProvider>
@@ -44,7 +45,18 @@ export default function App() {
                                 />
                             }
                         />
-                        <Route path='*' element={<NotFoundPage />} />
+                        <Route
+                            path='*'
+                            element={
+                                <ErrorPage
+                                    title='Oops!'
+                                    subTitle='404 - Page not found'
+                                    description='The page you are looking for does not exist. How you got here is a mystery, but you can click the button below to go back to the homepage.'
+                                    buttonText='Go to homepage'
+                                    buttonFunction={() => navigate('/')}
+                                />
+                            }
+                        />
                         <Route path='/register' element={<RegisterPage />} />
                         <Route path='/song/:id' element={<SongPage />} />
                         <Route path='/artist/:id' element={<ArtistPage />} />
