93857604fea70d4cf18031590aeeed58d95e1b0c
linnerudsander <sandesl@stud.ntnu.no>
2023-11-19 13:56:59 +0100

    test: snapshot test for LoginPage
    issue: #46

---
+++ frontend/src/tests/snapshot/LoginPage.test.tsx
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import LoginPage from '../../components/pages/LoginPage';
+import userReducer from '../../redux/reducers/userReducer';
+
+const mockStore = configureStore({
+    reducer: {
+        user: userReducer
+    },
+    preloadedState: {
+        user: {
+            loggedIn: false,
+            username: null
+        }
+    }
+});
+
+test('LoginPage snapshot', () => {
+    const { asFragment } = render(
+        <Provider store={mockStore}>
+            <MockedProvider addTypename={false}>
+                <Router>
+                    <LoginPage />
+                </Router>
+            </MockedProvider>
+        </Provider>
+    );
+    expect(asFragment()).toMatchSnapshot();
+});
