0ff01725270680f72d5b0522c4b357f0efebbfef
linnerudsander <sandesl@stud.ntnu.no>
2023-11-20 17:20:40 +0100

    docs: JSDoc on hooks and types
    issue: #73

--- frontend/src/hooks/useFavorites.ts
+++ frontend/src/hooks/useFavorites.ts
@@ -9,6 +9,19 @@ type Favorite = {
     targetId: number
 }
 
+/**
+ * Custom hook: `useFavorites`.
+ *
+ * This hook is used to fetch a user's favorite songs and artists from the database. 
+ * It handles the logic to separate the favorite songs and artists based on their type and then fetches their detailed information using GraphQL queries.
+ *
+ * @param {string | null} username - The username of the user whose favorites are being requested.
+ * @returns {Object} An object containing:
+ *                   - `dataSongs`: The detailed information of the user's favorite songs.
+ *                   - `dataArtists`: The detailed information of the user's favorite artists.
+ *                   - `loading`: A boolean indicating if any of the queries are still loading.
+ *                   - `error`: An error object that aggregates any errors from the GraphQL queries.
+ */
 export const useFavorites = (username: string | null) => {
     const [songFavorites, setSongFavorites] = useState<number[]>([])
     const [artistFavorites, setArtistFavorites] = useState<number[]>([])
