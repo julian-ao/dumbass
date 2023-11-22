9a710875ad6e040fb4d4f7db62b2102d0f2a7e61
jakobop <jakobop@stud.ntnu.no>
2023-11-22 20:56:37 +0100

    feat: Start writing tests for SearchBar component
    Star writing first few tests for SearchBar component
    
    Issue: #83

--- frontend/src/components/molecules/SearchBar.tsx
+++ frontend/src/components/molecules/SearchBar.tsx
@@ -202,7 +202,9 @@ const SearchBar = (props: SearchBarProps) => {
                 </button>
             </form>
             {showDropdown && isFocusing && (
-                <ul className='absolute top-full mt-1 w-full flex flex-col bg-white border border-gray-200 rounded-md shadow-lg'>
+                <ul
+                    id='dropdown-SearchBar'
+                    className='absolute top-full mt-1 w-full flex flex-col bg-white border border-gray-200 rounded-md shadow-lg'>
                     {filteredData.map((item, index) => (
                         <button
                             key={index}
--- frontend/src/tests/component/SearchBar.test.tsx
+++ frontend/src/tests/component/SearchBar.test.tsx
@@ -1,11 +1,53 @@
-//import HomePage from '../components/pages/HomePage'
-//import { render } from '@testing-library/react'
-import { test } from 'vitest'
+import { describe, test, expect } from 'vitest'
+import { fireEvent, getByPlaceholderText, render } from '@testing-library/react'
+import SearchBar from '../../components/molecules/SearchBar'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { MockedProvider, MockedResponse } from '@apollo/client/testing'
 import '@testing-library/jest-dom'
-//import { MemoryRouter } from 'react-router-dom'
 
-test('HomePage renders without crashing', () => {
-    true
+const mocks:
+    | readonly MockedResponse<Record<string, any>, Record<string, any>>[]
+    | undefined = []
+
+describe('SearchBar Component', () => {
+    test('renders without crashing', () => {
+        const { getByPlaceholderText } = render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <Router>
+                    <SearchBar />
+                </Router>
+            </MockedProvider>
+        )
+        expect(
+            getByPlaceholderText('Search for a song or artist')
+        ).toBeInTheDocument()
+    })
+
+    //test('dropdown becomes visible on valid search term', async () => {
+    //    // Simulate user typing a search term
+    //    const searchInput = getByPlaceholderText('Search for a song or artist')
+    //    fireEvent.change(searchInput, { target: { value: 'test' } })
+//
+    //    // Wait for the dropdown to become visible
+    //    const dropdown = await findByTestId('dropdown-SearchBar')
+    //    expect(dropdown).toBeInTheDocument()
+    //})
+
+    test('no dropdown when search term is empty', () => {
+        const { getByPlaceholderText, queryByTestId } = render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <Router>
+                    <SearchBar />
+                </Router>
+            </MockedProvider>
+        )
+
+        // Ensure the search term is empty
+        const searchInput = getByPlaceholderText('Search for a song or artist')
+        fireEvent.change(searchInput, { target: { value: '' } })
+
+        // Check that the dropdown is not present
+        const dropdown = queryByTestId('dropdown-SearchBar')
+        expect(dropdown).not.toBeInTheDocument()
+    })
 })
-
-// TODO
